bcf2bc55e2e321f7bb9c52cb0549f2fc54cac7e9
GG <GG1D29@users.noreply.github.com>
2025-11-27 13:46:59 +0700

    init commmit

---
+++ src/test/java/com/stanley/xie/cleanarchitecturedemo/CleanArchitectureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stanley.xie.cleanarchitecturedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
